d1a08c464ce10a0bac833b5878e12e669a6c47aa
MaetroAndrey <top1twink@list.ru>
2021-07-25 18:10:49 +0300

    Site
    Site

---
+++ mysite/news/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+from .models import News, Category
+
+
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
+class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'is_published', 'view', 'created_at', 'updated_at')
+    readonly_fields = ('view', 'created_at', 'updated_at')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
